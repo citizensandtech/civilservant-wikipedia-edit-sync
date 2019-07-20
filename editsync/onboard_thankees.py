b01069ef51e9e55bfc7f59ba4ffc6c2c1329a7d5
Max Klein <isalix@gmail.com>
2019-07-19 17:16:41 -0700

    new way to get the already rev ids

--- editsync/onboard_thankees.py
+++ editsync/onboard_thankees.py
@@ -249,6 +249,7 @@ class thankeeOnboarder():
 
         # add num_quality back into the dataframe
         self.db_session.commit()
+        time.sleep(10)
         num_quality_dfs = []
         for user_id in user_ids:
             num_quality = self.db_session.query(candidates).filter(candidates.lang == lang).filter(
@@ -302,12 +303,22 @@ class thankeeOnboarder():
         # do this in a user-oriented way, or a process-oriented way?
         # revisions of users
         small_user_df = pd.DataFrame({"user_id": [refresh_user.user_id]})
-        logging.info(f"starting to get quality edits for user {refresh_user.id}")
+        logging.info(f"starting to get quality edits for user{lang}-{refresh_user.user_id}")
         # already received revisions
-        already_revs_res = self.db_session.query(edits).filter(edits.lang == lang).filter(
-            edits.candidate_id == refresh_user.id).all()
-        already_revs = set([r.rev_id for r in already_revs_res])
-        logging.info(f"already have {len(already_revs)} revs for user {refresh_user.id}")
+
+        et_query_index = f'user:{lang}:{refresh_user.user_id}'
+        logging.info(f"exp thing query str is: {et_query_index}")
+        already_ets = self.db_session.query(ExperimentThing).filter(ExperimentThing.experiment_id == -10) \
+            .filter(ExperimentThing.query_index == et_query_index).all()
+
+        already_et_lang_revids = [already_et.id for already_et in already_ets]
+        # edit:en:906694307
+        already_revs = [int(s.split(':')[2]) for s in already_et_lang_revids]
+
+        # already_revs_res = self.db_session.query(edits).filter(edits.lang == lang).filter(
+        #     edits.candidate_id == refresh_user.id).all()
+        # already_revs = set([r.rev_id for r in already_revs_res])
+        logging.info(f"already have {len(already_revs)} revs for user {refresh_user.user_id}")
 
         new_user_revs = get_quality_edits_of_users(small_user_df, lang, self.wmf_con, exclusion_rev_ids=already_revs)
         # revisions needing getting = revs - already
@@ -353,7 +364,6 @@ class thankeeOnboarder():
         - update last k edits of included, not completed users
         - determine quality of edits
         - get editDisplay data
-        :param user:
         :param lang:
         :return:
         """
